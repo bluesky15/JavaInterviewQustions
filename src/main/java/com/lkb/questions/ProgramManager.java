fb0cea190fd804df6d7b613e82abcfdbe2802d94
LALIT KUMAR BEHERA <leo@leo.local>
2018-05-20 20:58:09 +0530

    project file added

---
+++ src/main/java/com/lkb/questions/ProgramManager.java
@@ -0,0 +1,10 @@
+package com.lkb.questions;
+
+import java.util.List;
+
+public class ProgramManager {
+    public List<String> shortStringArray(List<String> list){
+
+        return null;
+    }
+}
