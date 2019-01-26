8e6a6ea6b50f3f50d5134ac7f51fd359e90a3d94
Lalit Behera <lalit143.er@gmail.com>
2019-01-26 20:34:23 +0530

    thread even odd program added

---
+++ src/main/java/com/lkb/basic/thread/OddEvenJob.java
@@ -0,0 +1,71 @@
+package com.lkb.basic.thread;
+
+public class OddEvenJob implements Runnable {
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+
+    public OddEvenJob(int max, Printer print, boolean isEvenNumber) {
+        this.max = max;
+        this.print = print;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+    @Override
+    public void run() {
+        int number = isEvenNumber ? 0 : 1;
+        while (number < max) {
+            if (isEvenNumber) {
+                print.printEven(number);
+            } else {
+                print.printOdd(number);
+            }
+            number += 2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Printer print = new Printer();
+        Thread t1 = new Thread(new OddEvenJob(10, print, false), "Odd");
+        Thread t2 = new Thread(new OddEvenJob(10, print, true), "Even");
+        t1.start();
+        t2.start();
+
+
+    }
+
+
+}
+
+class Printer {
+    private volatile boolean isOdd;
+
+    synchronized void printEven(int number) {
+        while (isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        //System.out.print(number);
+        isOdd = true;
+        notify();
+    }
+
+    synchronized void printOdd(int number) {
+        while (!isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        //System.out.print(number);
+        isOdd = false;
+        notify();
+    }
+}
