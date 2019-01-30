c81ad0c70655605e3036e4b5e5bcae8241b5028f
Lalit Behera <lalit143.er@gmail.com>
2019-01-30 10:36:08 +0530

    builder design pattern demo added

---
+++ src/main/java/com/lkb/basic/designpattern/factory/ComputerFactory.java
@@ -0,0 +1,11 @@
+package com.lkb.basic.designpattern.factory;
+
+public class ComputerFactory {
+
+    public static Computer getComputer(String type,String ram, String hdd, String cpu){
+        if("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
+        else if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+        else throw new RuntimeException("Unsupported class type");
+    }
+
+}
