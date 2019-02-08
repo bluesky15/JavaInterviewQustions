5511e61c8408700b3c8a5b4889c04d7a56d55092
Lalit Behera <lalit143.er@gmail.com>
2019-02-08 10:46:07 +0530

    creating new scope using coroutinescope

--- src/main/java/com/lkb/questions/DemoKotlin.kt
+++ src/main/java/com/lkb/questions/DemoKotlin.kt
@@ -1,35 +1,21 @@
 package com.lkb.questions
 
 import kotlinx.coroutines.*
-//
-//fun main() {
-//    GlobalScope.launch {
-//        // launch new coroutine in background and continue
-//        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-//        println("World!") // print after delay
-//    }
-//    println("Hello,") // main thread continues while coroutine is delayed
-//    //Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
-//    runBlocking {
-//        delay(
-//                2000L
-//        )
-//    }
-//}
-
-//fun main()= runBlocking<Unit>{
-//    val job = GlobalScope.launch {
-//        delay(1000L)
-//        println("world")
-//    }
-//    println("Hello")
-//    job.join()
-//}
-// better example
 fun main() = runBlocking { // this: CoroutineScope
-    launch { // launch new coroutine in the scope of runBlocking
-        delay(1000L)
-        println("World!")
+    launch {
+        delay(200L)
+        println("Task from runBlocking")
     }
-    println("Hello,")
+
+    coroutineScope { // Creates a new coroutine scope
+        launch {
+            delay(500L)
+            println("Task from nested launch")
+        }
+
+        delay(100L)
+        println("Task from coroutine scope") // This line will be printed before nested launch
+    }
+
+    println("Coroutine scope is over") // This line is not printed until nested launch completes
 }
